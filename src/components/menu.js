b92a952cd3bfc502d9e280c34ad5d4fe4ccf6cb1
Juniër <102233050+DhuJUICE@users.noreply.github.com>
2025-03-16 20:48:27 -0800

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/menu.css'; // Import your CSS file
+import { Link } from 'react-router-dom'; // Import Link for routing
 
 const Menu = () => {
   const [userMenuVisible, setUserMenuVisible] = useState(false);
@@ -122,20 +123,28 @@ const Menu = () => {
           </div>
         </div>
         <div className="nav">
-          <ul>
-            <li>
-              <a href="/menu" className="menu">Menu</a>
-            </li>
-            <li>
-              <a href="/partnerships" className="partnerships">Partnerships</a>
-            </li>
-            <li>
-              <a href="/about" className="about-us">About Us</a>
-            </li>
-            <li>
-              <a href="/contact" className="contact-us">Contact Us</a>
-            </li>
-          </ul>
+                  <ul>
+                    <li>
+                      <Link to="/menu" className="menu">
+                        Menu
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/partnerships" className="partnerships">
+                        Partnerships
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/about" className="about-us">
+                        About Us
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/contact" className="contact-us">
+                        Contact Us
+                      </Link>
+                    </li>
+                  </ul>
         </div>
       </header>
 
