e890daab13b248ce0740a8c7e7632c8b0e65d174
Juniër <102233050+DhuJUICE@users.noreply.github.com>
2025-04-16 00:33:20 +0200

    Update footer.js

--- src/components/sectionComponents/footer.js
+++ src/components/sectionComponents/footer.js
@@ -8,68 +8,12 @@ const Footer = () => {
     <div>
       <div className="container">
         <div className="section">
-          <Link to="/menu">Menu</Link>
-        </div>
-        <div className="section">
-          <Link to="/values">Our Values</Link>
-        </div>
-        <div className="section">
           <Link to="/contact">Contact Us</Link>
         </div>
         <div className="section">
           <Link to="/about">About Us</Link>
         </div>
       </div>
-      {/*
-      <div className="links">
-        <div className="social-links">
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-icon"
-          >
-            <img
-              src={`${process.env.PUBLIC_URL}/images/instragram.jpg`}
-              alt="Instagram"
-            />
-          </a>
-          <a
-            href="https://www.facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-icon"
-          >
-            <img
-              src={`${process.env.PUBLIC_URL}/images/facebook.jpg`}
-              alt="Facebook"
-            />
-          </a>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-icon"
-          >
-            <img
-              src={`${process.env.PUBLIC_URL}/images/x.png`}
-              alt="X (formerly Twitter)"
-            />
-          </a>
-          <a
-            href="https://www.youtube.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-icon"
-          >
-            <img
-              src={`${process.env.PUBLIC_URL}/images/youtube.png`}
-              alt="YouTube"
-            />
-          </a>
-        </div>
-      </div>
-      */}
 
       <div className="copyrights">
       <p>
